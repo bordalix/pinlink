45cede4eda54313ba2274cf210e673deafd6503f
João Bordalo <bordalix@users.noreply.github.com>
2024-02-18 17:00:34 +0000

    activate relays

--- publish.sh
+++ publish.sh
@@ -4,4 +4,4 @@ source ./.env.local # for seckey
 relays="wss://nos.lol wss://relay.primal.net wss://relay.damus.io wss://relay.nostr.band wss://nostr.bitcoiner.social"
 
 # verify on https://snort.social/npub16c0l92flapw0qrrkx4wk0y63e252yvr58u4rr832trnfpqgrfwnqre85ac
-nak event -c "`cat event.txt`" --sec $seckey # $relays
+nak event -c "`cat event.txt`" --sec $seckey $relays
