1ffb28270a8b6cab3dcccac1bce03997df777e75
João Bordalo <bordalix@users.noreply.github.com>
2024-02-19 11:10:38 +0000

    bug fix

--- fetch.sh
+++ fetch.sh
@@ -2,8 +2,8 @@
 
 source ./.env.local # for authtoken
 
-os=${uname}
-if [ os == 'Darwin' ]; then
+os=$(uname)
+if [ $os == 'Darwin' ]; then
   yesterday=$(date -v-1d +%FT%TZ)
 else
   yesterday=$(date -d "1 day ago" +%FT%TZ)
